c4f6eca0eb8e97993ad41ec23a1640d6df666134
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-06-24 21:59:49 -0700

    journalEntries

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
-
+const JournalEntryController = require('../controllers/journalEntryController');
 // Endpoint: GET /status
 router.get('/status', AppController.getStatus);
 
+// Journal Entry Routes
+router.post('/JournalEntries', JournalEntryController.createJournalEntry);
+router.get('/JournalEntries/user/:userId', JournalEntryController.getJournalEntriesByUser);
+router.get('/journalEntries/:id', JournalEntryController.getJournalEntryById);
+router.put('/JournalEntries/:id', JournalEntryController.updateJournalEntry);
+router.delete('/JournalEntries/:id', JournalEntryController.deleteJournalEntry);
 
 module.exports = router;
