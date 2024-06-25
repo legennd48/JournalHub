58408c680db0be2bce1645d69d6836b2c253229e
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-25 15:35:28 +0100

    fixes to package.json

--- src/server.js
+++ src/server.js
@@ -4,13 +4,9 @@ import routes from './routes/index';
 import dbClient from './utils/db';
 
 // Import necessary modules
-const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const userRoutes = require('./src/routes/index');
-const dbClient = require('./utils/db');
-
 
 // Load environment variables from .env file
 dotenv.config();
@@ -35,7 +31,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // Route handling
-app.use('/api/users', userRoutes);
+app.use('/api/users', routes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
